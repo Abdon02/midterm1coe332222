6a20d9dbca95fb2725941556b21104ca2db2a830
Abdon Verdejo-Parada <averdejo02@utexas.edu>
2022-03-17 11:40:12 -0500

    started the app file

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+#Creating an intance of the class flask in the varaibel called app
+app = Flask(__name__)
+
+#Creating the routes
+
+#Creating the main function
+def main():
+    pass
+
+
+if __name__ == "__main___":
+    main()
