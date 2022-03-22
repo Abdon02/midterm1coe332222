1b1ca60d8892e1f860d3f2ea525e5aa55b514665
Abdon Verdejo-Parada <averdejo02@utexas.edu>
2022-03-21 19:00:06 -0500

    The made a few changes

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-from app import welcome_message
+import app
 import pytest 
 from flask import Flask, request, jsonify
 
 #Testing the function that prints out the welcome message 
 def test_welcome_message():
-    assert isinstance(welcome_message(), str) == True
+    assert isinstance(app.welcome_message(), str) == True
+
+
+    
